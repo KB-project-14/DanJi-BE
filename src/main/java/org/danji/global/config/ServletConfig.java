2af9d6e59ab118b7b63150a3c97f161d1d385c2d
jeonkyujin <954qew@naver.com>
2025-07-17 00:52:30 +0900

    feat: 전역예외 처리 및 에러 메시지 정의

--- src/main/java/org/danji/global/config/ServletConfig.java
+++ src/main/java/org/danji/global/config/ServletConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
+import org.springframework.web.servlet.DispatcherServlet;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
@@ -58,6 +59,13 @@ public class ServletConfig implements WebMvcConfigurer {
     public MultipartResolver multipartResolver() {
         StandardServletMultipartResolver resolver = new StandardServletMultipartResolver();
         return resolver;
+    }
+
+    @Bean
+    public DispatcherServlet dispatcherServlet() {
+        DispatcherServlet servlet = new DispatcherServlet();
+        servlet.setThrowExceptionIfNoHandlerFound(true);  // ✅ 여기에
+        return servlet;
     }
 
 
