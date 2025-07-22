e43d4137fef9e042155e0a076da6833c875fb762
yellowmodric <kyr7091@naver.com>
2025-07-22 18:56:17 +0900

    feat: 지역 단건 조회 mapper까지 완료

--- src/main/java/org/danji/region/mapper/RegionMapper.java
+++ src/main/java/org/danji/region/mapper/RegionMapper.java
@@ -5,5 +5,8 @@ import org.danji.region.domain.RegionVO;
 import java.util.List;
 
 public interface RegionMapper {
+    //지역 리스트 조회
     List<RegionVO> findAll();
+    //지역 단건 조
+    RegionVO findById(Long regionId);
 }
