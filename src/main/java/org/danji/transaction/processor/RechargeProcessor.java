d7e3343ecdbb74390386bfc2b1becba77678e1aa
jeonkyujin <954qew@naver.com>
2025-07-23 16:26:32 +0900

    refactor: 지역화폐 충전 로직 수정

--- src/main/java/org/danji/transaction/processor/RechargeProcessor.java
+++ src/main/java/org/danji/transaction/processor/RechargeProcessor.java
@@ -43,8 +43,8 @@ public class RechargeProcessor implements TransferProcessor {
         // TODO Authentication 객체에서 userID 정보 꺼내는 코드 작성
         //-------------------------------------------
         UUID userId = UUID.randomUUID();
-        // userId로 메인 지갑 찾기
-        WalletVO mainWalletVO = walletMapper.getCashWalletByUserId(userId).orElseThrow(() -> new WalletException(ErrorCode.WALLET_NOT_FOUND));
+        // transferDTO의 fromWalletId 로 메인지갑 불러오기
+        WalletVO mainWalletVO = walletMapper.getWalletByUUId(transferDTO.getFromWalletId()).orElseThrow(() -> new WalletException(ErrorCode.WALLET_NOT_FOUND));
         // 요청금액보다 메인 지갑의 잔액이 작다면 예외 터뜨리기
         // 수수료 1% 도 감안해서 계산
         if (mainWalletVO.getBalance() + mainWalletVO.getBalance() * RECHARGE_FEE_RATE < transferDTO.getAmount()) {
@@ -58,13 +58,12 @@ public class RechargeProcessor implements TransferProcessor {
 
         if (localCurrencyVO.getBenefitType() == BenefitType.BONUS_CHARGE) {
             //요청금액 에 incentive 비율을 합한 금액으로 업데이트
-            int rechargeAmount = transferDTO.getAmount() + localCurrencyVO.getPercentage() * transferDTO.getAmount();
-            walletMapper.updateWalletBalance(mainWalletVO.getWalletId(), -(int) (mainWalletVO.getBalance() + mainWalletVO.getBalance() * RECHARGE_FEE_RATE));
-            walletMapper.updateWalletBalance(LocalCurrencyWalletVO.getWalletId(), rechargeAmount);
+            walletMapper.updateWalletBalance(mainWalletVO.getWalletId(), -transferDTO.getAmount());
+            walletMapper.updateWalletBalance(LocalCurrencyWalletVO.getWalletId(), (transferDTO.getAmount() - (int) (transferDTO.getAmount() - transferDTO.getAmount() * RECHARGE_FEE_RATE)) * localCurrencyVO.getPercentage());
         } else if (localCurrencyVO.getBenefitType() == BenefitType.CASHBACK) {
             // 요청 금액 업데이트 시키기
-            walletMapper.updateWalletBalance(mainWalletVO.getWalletId(), -(int) (mainWalletVO.getBalance() + mainWalletVO.getBalance() * RECHARGE_FEE_RATE));
-            walletMapper.updateWalletBalance(LocalCurrencyWalletVO.getWalletId(), transferDTO.getAmount());
+            walletMapper.updateWalletBalance(mainWalletVO.getWalletId(), -transferDTO.getAmount());
+            walletMapper.updateWalletBalance(LocalCurrencyWalletVO.getWalletId(), (int) (transferDTO.getAmount() - transferDTO.getAmount() * RECHARGE_FEE_RATE));
             //7일 뒤에 캐시백에 대한 부분 업데이트
 
             // TODO converter로 구현한거 빌더패턴으로 바꾸기
@@ -77,12 +76,11 @@ public class RechargeProcessor implements TransferProcessor {
         //메인지갑 기준
         TransactionVO mainTx = transactionConverter.toTransactionVO(
                 UUID.randomUUID(), mainWalletVO.getWalletId(), LocalCurrencyWalletVO.getWalletId(),
-                mainWalletVO.getBalance(), (int) (mainWalletVO.getBalance() - transferDTO.getAmount() - transferDTO.getAmount() * RECHARGE_FEE_RATE),
+                mainWalletVO.getBalance(), mainWalletVO.getBalance() - transferDTO.getAmount(),
                 transferDTO.getAmount(), Direction.EXPENSE, Type.CHARGE, "메인지갑(출금) -> 지역화폐 지갑", mainWalletVO.getWalletId());
         int successMainWalletCount = transactionMapper.insert(mainTx);
 
         if (successMainWalletCount != 1) {
-            log.error("Main wallet transaction insert failed. userId={}, amount={}", userId, transferDTO.getAmount() + transferDTO.getAmount() * RECHARGE_FEE_RATE);
             throw new TransactionException(ErrorCode.TRANSACTION_SAVE_FAILED_MAIN);
         }
         //지역화폐 기준
@@ -91,11 +89,10 @@ public class RechargeProcessor implements TransferProcessor {
         if (localCurrencyVO.getBenefitType() == BenefitType.BONUS_CHARGE) {
             localTx = transactionConverter.toTransactionVO(
                     UUID.randomUUID(), LocalCurrencyWalletVO.getWalletId(), mainWalletVO.getWalletId(),
-                    LocalCurrencyWalletVO.getBalance(), (int) (LocalCurrencyWalletVO.getBalance() + transferDTO.getAmount() + transferDTO.getAmount() * RECHARGE_FEE_RATE),
-                    transferDTO.getAmount(), Direction.INCOME, Type.CHARGE, "메인지갑 -> 지역화폐 지갑(입금)", LocalCurrencyWalletVO.getWalletId());
+                    LocalCurrencyWalletVO.getBalance(), LocalCurrencyWalletVO.getBalance() + (transferDTO.getAmount() - (int) (transferDTO.getAmount() - transferDTO.getAmount() * RECHARGE_FEE_RATE)) * localCurrencyVO.getPercentage(),
+                    (transferDTO.getAmount() - (int) (transferDTO.getAmount() - transferDTO.getAmount() * RECHARGE_FEE_RATE)) * localCurrencyVO.getPercentage(), Direction.INCOME, Type.CHARGE, "메인지갑 -> 지역화폐 지갑(입금)", LocalCurrencyWalletVO.getWalletId());
             int successLocalCurrencyWalletCount = transactionMapper.insert(localTx);
             if (successLocalCurrencyWalletCount != 1) {
-                log.error("Incentive Local wallet transaction insert failed. userId={}, amount={}", userId, transferDTO.getAmount() + transferDTO.getAmount() * localCurrencyVO.getPercentage());
                 throw new TransactionException(ErrorCode.TRANSACTION_SAVE_FAILED_LOCAL);
             }
         }
@@ -103,11 +100,10 @@ public class RechargeProcessor implements TransferProcessor {
         else if (localCurrencyVO.getBenefitType() == BenefitType.CASHBACK) {
             localTx = transactionConverter.toTransactionVO(
                     UUID.randomUUID(), LocalCurrencyWalletVO.getWalletId(), mainWalletVO.getWalletId(),
-                    LocalCurrencyWalletVO.getBalance(), (LocalCurrencyWalletVO.getBalance() + transferDTO.getAmount()),
-                    transferDTO.getAmount(), Direction.INCOME, Type.CHARGE, "메인지갑 -> 지역화폐 지갑(입금)", LocalCurrencyWalletVO.getWalletId());
+                    LocalCurrencyWalletVO.getBalance(), (LocalCurrencyWalletVO.getBalance() + (int) (transferDTO.getAmount() - transferDTO.getAmount() * RECHARGE_FEE_RATE)),
+                    (int) (transferDTO.getAmount() - transferDTO.getAmount() * RECHARGE_FEE_RATE), Direction.INCOME, Type.CHARGE, "메인지갑 -> 지역화폐 지갑(입금)", LocalCurrencyWalletVO.getWalletId());
             int successLocalCurrencyWalletCount = transactionMapper.insert(localTx);
             if (successLocalCurrencyWalletCount != 1) {
-                log.error("Local wallet transaction insert failed. userId={}, amount={}", userId, transferDTO.getAmount());
                 throw new TransactionException(ErrorCode.TRANSACTION_SAVE_FAILED_LOCAL);
             }
         }
