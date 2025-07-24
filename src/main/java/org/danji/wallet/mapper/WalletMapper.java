94ada7f59bca27f82d694020627f183f0af8fa11
jeonkyujin <954qew@naver.com>
2025-07-24 11:45:56 +0900

    feat: 지역화폐 환불 기능 구현

--- src/main/java/org/danji/wallet/mapper/WalletMapper.java
+++ src/main/java/org/danji/wallet/mapper/WalletMapper.java
@@ -11,7 +11,6 @@ import java.util.Optional;
 import java.util.UUID;
 
 public interface WalletMapper {
-    Optional<WalletVO> getCashWalletByUserId(UUID userId);
 
     Optional<WalletVO> getWalletByUUId(UUID walletId);
 
