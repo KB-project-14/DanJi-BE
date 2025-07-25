a7f5b9f87294a35dcc80485eb748d95f91866e82
yellowmodric <kyr7091@naver.com>
2025-07-25 10:52:33 +0900

    refactor: migration 없앰

--- database.sql
+++ database.sql
@@ -1,5 +1,24 @@
 use danji;
 
+INSERT INTO local_currency
+VALUES (UUID_TO_BIN('11111111-1111-1111-1111-111111111111'), 26001, '동백전', 'CASHBACK', 500000, 5, NOW(), NOW()),
+       (UUID_TO_BIN('22222222-2222-2222-2222-222222222222'), 26002, '동백전', 'CASHBACK', 500000, 5, NOW(), NOW()),
+       (UUID_TO_BIN(UUID()), 26003, '동백전', 'CASHBACK', 500000, 5, NOW(), NOW()),
+       (UUID_TO_BIN(UUID()), 26004, '동백전', 'CASHBACK', 500000, 5, NOW(), NOW()),
+       (UUID_TO_BIN(UUID()), 26005, '동백전', 'CASHBACK', 500000, 5, NOW(), NOW()),
+       (UUID_TO_BIN(UUID()), 26006, '동백전', 'CASHBACK', 500000, 5, NOW(), NOW()),
+       (UUID_TO_BIN(UUID()), 26007, '동백전', 'CASHBACK', 500000, 5, NOW(), NOW()),
+       (UUID_TO_BIN(UUID()), 26008, '동백전', 'CASHBACK', 500000, 5, NOW(), NOW()),
+       (UUID_TO_BIN(UUID()), 26009, '동백전', 'CASHBACK', 500000, 5, NOW(), NOW()),
+       (UUID_TO_BIN(UUID()), 26010, '동백전', 'CASHBACK', 500000, 5, NOW(), NOW()),
+       (UUID_TO_BIN(UUID()), 26011, '동백전', 'CASHBACK', 500000, 5, NOW(), NOW()),
+       (UUID_TO_BIN(UUID()), 26012, '동백전', 'CASHBACK', 500000, 5, NOW(), NOW()),
+       (UUID_TO_BIN(UUID()), 26013, '동백전', 'CASHBACK', 500000, 5, NOW(), NOW()),
+       (UUID_TO_BIN(UUID()), 26014, '동백전', 'CASHBACK', 500000, 5, NOW(), NOW()),
+       (UUID_TO_BIN(UUID()), 26015, '동백전', 'CASHBACK', 500000, 5, NOW(), NOW()),
+       (UUID_TO_BIN(UUID()), 26016, '동백전', 'CASHBACK', 500000, 5, NOW(), NOW());
+
+
 INSERT INTO available_merchant
 VALUES
     (UUID_TO_BIN(UUID()),'아이즈광고기획', '부산광역시 사상구 사상로 467(모라동)', 0.0, 0.0, '기타', UUID_TO_BIN('11111111-1111-1111-1111-111111111111'), NOW(), NOW()),
