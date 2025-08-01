861a1fa2dbed32866b2b7b27d0bb784736921efa
jihun <wlgns679@gmail.com>
2025-08-01 13:48:34 +0900

    fix: 지역화폐 단건 조회 로직 수정

--- src/main/java/org/danji/localCurrency/service/LocalCurrencyService.java
+++ src/main/java/org/danji/localCurrency/service/LocalCurrencyService.java
@@ -4,12 +4,13 @@ import org.danji.localCurrency.dto.LocalCurrencyDTO;
 import org.danji.localCurrency.dto.LocalCurrencyFilterDTO;
 
 import java.util.List;
+import java.util.UUID;
 
 public interface LocalCurrencyService {
 
     LocalCurrencyDTO createLocalCurrency(LocalCurrencyDTO localCurrencyDTO);
 
-    LocalCurrencyDTO getLocalCurrencyByRegionId(Long RegionId);
+    LocalCurrencyDTO getLocalCurrency(UUID localCurrencyId);
 
     List<LocalCurrencyDTO> getLocalCurrencyList(LocalCurrencyFilterDTO filter);
 
