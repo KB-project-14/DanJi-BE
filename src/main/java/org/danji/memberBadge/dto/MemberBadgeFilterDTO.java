85e458c030f2dbcc51a9224380aa26e53f10eb40
jihun <wlgns679@gmail.com>
2025-08-07 11:40:39 +0900

    fix: 지갑 순서 변경 로직 수정

--- src/main/java/org/danji/memberBadge/dto/MemberBadgeFilterDTO.java
+++ src/main/java/org/danji/memberBadge/dto/MemberBadgeFilterDTO.java
@@ -1,17 +1,21 @@
 package org.danji.memberBadge.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Builder;
 import lombok.Data;
 import org.danji.badge.enums.BadgeType;
 import org.danji.memberBadge.enums.BadgeGrade;
 
+import javax.validation.constraints.NotNull;
 import java.util.UUID;
 
 @Data
 @Builder
 public class MemberBadgeFilterDTO {
+
+    @NotNull(message = "memberId 값은 필수입니다.")
+    @ApiModelProperty(value = "member UUID", required = true, example = "00000000-0000-0000-0000-000000000000")
     private UUID memberId;
-    private UUID badgeId;
     private BadgeGrade badgeGrade;
     private BadgeType badgeType;
     private Long regionId;
